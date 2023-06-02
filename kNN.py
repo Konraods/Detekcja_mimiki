387902a4b29b03ad37c4704c0ffa3858c3967f3b
Konraods <101873060+Konraods@users.noreply.github.com>
2023-06-02 21:48:40 +0200

    Update kNN.py
    Changed methods to create model once and be able to use it with multiple images without creating new model

--- kNN.py
+++ kNN.py
@@ -7,10 +7,9 @@ from sklearn.model_selection import train_test_split
 from imutils import paths
 
 class k_NN:
-    """Konstruktor ze sciezka do folderow ze zdjeciami, sciezka do badanego zdjecia
+    """Konstruktor ze sciezka do folderow ze zdjeciami
     oraz preprocesorem i dataloaderem do modyfikacji zdjec"""
-    def __init__(self, path_to_image, path_to_dictionary):
-        self.path_to_image = path_to_image
+    def __init__(self, path_to_dictionary):
         self.path_to_dictionary = path_to_dictionary
         self.preproc = Preprocessor(224, 224)
         self.loader = DataLoader(preprocessor=[self.preproc])
@@ -18,32 +17,32 @@ class k_NN:
     """Metoda do tworzenia modelu kNN"""
     def kNN_model(self):
         #Przeksztalcanie zdjec na jednowymiarowe szeregi dla klasyfikatora kNN, tworzenie listy etykiet zdjec
-        (images, labels) = self.loader.data_load_kNN(list(paths.list_images(self.path_to_dictionary)))
+        (images, labels) = self.loader.data_load_kNN(input_path=list(paths.list_images(self.path_to_dictionary)))
         images = images.reshape((images.shape[0], 224*224*3))
-        
+
         #Zmiana etykiet z lancuchow znakow na cyfry
         labels_encoded = LabelEncoder()
         labels = labels_encoded.fit_transform(labels)
-        
+
         #Podzielenie zdjec na zbiory treningowy i testowy w stosunku 4:1
         (trainX, testX, trainY, testY) = train_test_split(images, labels,
             test_size=0.25, random_state=55)
-        
+
         #Stworzenie modelu
         model = KNeighborsClassifier(n_neighbors=15)
-        model.fit(trainX, trainY)
-        
+        model.fit(X=trainX, y=trainY)
+
         return model
 
     """Metoda do predykcji emocji z wybranego zdjecia"""
-    def kNN_prediction(self, model):
+    def kNN_prediction(self, model, path_to_image):
         # Przeksztalcanie zdjecia na jednowymiarowe szereg dla klasyfikatora kNN, tworzenie etykiety zdjecia
-        (image, label) = self.loader.data_load_kNN(self.path_to_image)
+        (image, label) = self.loader.data_load_kNN(input_path=path_to_image)
         image = image.reshape((image.shape[0], 224 * 224 * 3))
 
         #Slownik do odszyfrowania etykiety
         class_mapping = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happiness', 4: 'sadness'}
-        
+
         #Detekcja mimiki na podstawie modelu
         prediction = model.predict(image)[0]
         #Odszyfrowanie etykiety na podstawie slownika
