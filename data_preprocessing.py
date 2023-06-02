8ba134c4004f23e4cc24b2a3500872464dd03146
Konraods <101873060+Konraods@users.noreply.github.com>
2023-06-02 21:28:44 +0200

    Update data_preprocessing.py
    Added comments to code

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,12 +2,13 @@
 import cv2
 
 class Preprocessor:
+    """Konstruktor z szerokoscia, wysokoscia i interpolacja"""
     def __init__(self, width, height, interpolation=cv2.INTER_AREA):
         #Przechowuje docelowe wymiary do zmienienia rozmiaru obrazow
         self.width = width
         self.height = height
         self.interpolation = interpolation
-
+    
+    """Metoda do zmiany wymiarow zdjec"""
     def preprocessing(self, image):
-        #Metoda do zmieniania rozmiarow zdjec
         return cv2.resize(src=image, dsize=(self.width, self.height), interpolation=self.interpolation)
