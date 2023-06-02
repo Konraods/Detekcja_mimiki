6fb11da1cf753385a9db1ffcd6362866acd3f4f5
Konraods <101873060+Konraods@users.noreply.github.com>
2023-06-02 20:57:04 +0200

    Update kNN.py
    Facial recognition will happen in this script instead of GUI.py

--- kNN.py
+++ kNN.py
@@ -1,33 +1,45 @@
 #Klasyfikator k-NN
-from GUI import MainWindow
+
 from data_loader import DataLoader
 from data_preprocessing import Preprocessor
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 from imutils import paths
 
-#knn_path = "input"
-knn_path = MainWindow.select_image
-#Sciezka do zdjec
-input_path = list(paths.list_images(knn_path))
 
-preproc = Preprocessor(224, 224)
-loader = DataLoader(preprocessor=[preproc])
-(images, labels) = loader.data_load_kNN(input_path)
-images = images.reshape((images.shape[0], 224*224*3))
+class k_NN:
 
-labels_encoded = LabelEncoder()
-labels = labels_encoded.fit_transform(labels)
+    def __init__(self, path_to_image, path_to_dictionary):
 
-class_mapping = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happiness', 4: 'sadness'}
+        self.path_to_image = path_to_image
+        self.path_to_dictionary = path_to_dictionary
 
-(trainX, testX, trainY, testY) = train_test_split(images, labels,
-    test_size=0.25, random_state=55)
 
-model = KNeighborsClassifier(n_neighbors=15)
-model.fit(trainX, trainY)
+    def kNN_model(self):
+        input_directory = list(paths.list_images(self.path_to_dictionary))
 
-#print(classification_report(testY, model.predict(testX),
-    #target_names=labels_encoded.classes_))
+        preproc = Preprocessor(224, 224)
+        loader = DataLoader(preprocessor=[preproc])
+
+        (images, labels) = loader.data_load_kNN(input_directory)
+        images = images.reshape((images.shape[0], 224*224*3))
+
+        labels_encoded = LabelEncoder()
+        labels = labels_encoded.fit_transform(labels)
+
+        class_mapping = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happiness', 4: 'sadness'}
+
+        (trainX, testX, trainY, testY) = train_test_split(images, labels,
+            test_size=0.25, random_state=55)
+
+        model = KNeighborsClassifier(n_neighbors=15)
+        model.fit(trainX, trainY)
+
+        (image, label) = loader.data_load_kNN(self.path_to_image)
+        image = image.reshape((image.shape[0], 224 * 224 * 3))
+
+        prediction = model.predict(image)[0]
+        predicted_label = class_mapping.get(prediction)
+
+        return label, predicted_label
