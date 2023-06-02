ee4703d394f80b81dfc7dadd091ed05679f3a5b0
Konraods <101873060+Konraods@users.noreply.github.com>
2023-06-02 21:13:34 +0200

    Update 2 kNN.py
    Added method kNN_prediction() to predict results
    Removed prediction from kNN_model()

--- kNN.py
+++ kNN.py
@@ -9,26 +9,20 @@ from imutils import paths
 
 
 class k_NN:
-
+    #Konstruktor ze sciezka do folderow ze zdjeciami oraz sciezka do badanego zdjecia
     def __init__(self, path_to_image, path_to_dictionary):
-
         self.path_to_image = path_to_image
         self.path_to_dictionary = path_to_dictionary
-
+        self.preproc = Preprocessor(224, 224)
+        self.loader = DataLoader(preprocessor=[self.preproc])
 
     def kNN_model(self):
-        input_directory = list(paths.list_images(self.path_to_dictionary))
 
-        preproc = Preprocessor(224, 224)
-        loader = DataLoader(preprocessor=[preproc])
-
-        (images, labels) = loader.data_load_kNN(input_directory)
+        (images, labels) = self.loader.data_load_kNN(list(paths.list_images(self.path_to_dictionary)))
         images = images.reshape((images.shape[0], 224*224*3))
 
         labels_encoded = LabelEncoder()
         labels = labels_encoded.fit_transform(labels)
-
-        class_mapping = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happiness', 4: 'sadness'}
 
         (trainX, testX, trainY, testY) = train_test_split(images, labels,
             test_size=0.25, random_state=55)
@@ -36,8 +30,14 @@ class k_NN:
         model = KNeighborsClassifier(n_neighbors=15)
         model.fit(trainX, trainY)
 
-        (image, label) = loader.data_load_kNN(self.path_to_image)
+        return model
+
+    def kNN_prediction(self, model):
+
+        (image, label) = self.loader.data_load_kNN(self.path_to_image)
         image = image.reshape((image.shape[0], 224 * 224 * 3))
+
+        class_mapping = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happiness', 4: 'sadness'}
 
         prediction = model.predict(image)[0]
         predicted_label = class_mapping.get(prediction)
